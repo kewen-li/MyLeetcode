70aa74062fc6363fcb1df4e40f1d2c2d510ead8e
Kewen Li <799150383@qq.com>
2022-06-03 01:29:37 -0700

    Array#1: Hash  Table Method

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -8,18 +8,24 @@
 from typing import List
 
 ### Two-Pointer
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):    
-                if (nums[i]+nums[j]) == target:
-                    return [i,j]
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):    
+#                 if (nums[i]+nums[j]) == target:
+#                     return [i,j]
 
 
 ### Hash table
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         seen = {}
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for index, value in enumerate(nums):
+            remain = target - value
+            if remain in seen:
+                return [index, seen[remain]]
+            else:
+                seen[value] = index 
         
         
 # @lc code=end
