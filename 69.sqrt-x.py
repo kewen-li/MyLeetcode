8888990978e56b997c57b3393608bc4575d24338
Kewen Li <799150383@qq.com>
2022-06-18 15:08:36 -0700

    Revisde the logic for the special case

--- 69.sqrt-x.py
+++ 69.sqrt-x.py
@@ -5,25 +5,22 @@
 #
 
 # @lc code=start
+
 class Solution:
     def mySqrt(self, x: int) -> int:
         left = 0
         right = x
         while (left <= right):
             middle = (left+right)//2
-            print("left:",left)
-            print("right:",right)
-            print("middle:",middle)
-            if (middle*middle) < x:
-                left = middle+1
+            if (middle*middle) <= x < (middle+1)*(middle+1):
+                return middle 
             elif (middle*middle) > x:
                 right = middle-1
             else:
-                return middle
-        return left
+                left = middle+1
                 
 s = Solution()
-print(s.mySqrt(8))
+print(s.mySqrt(26))
         
 # @lc code=end
 
