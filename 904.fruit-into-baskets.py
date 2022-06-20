f99188db79facce4bdfc286e089a93ae7195c537
Kewen Li <799150383@qq.com>
2022-06-20 01:30:54 -0700

    My imcomplete version 904

---
+++ 904.fruit-into-baskets.py
@@ -0,0 +1,102 @@
+#
+# @lc app=leetcode id=904 lang=python3
+#
+# [904] Fruit Into Baskets
+#
+# https://leetcode.com/problems/fruit-into-baskets/description/
+#
+# algorithms
+# Medium (42.69%)
+# Likes:    1148
+# Dislikes: 87
+# Total Accepted:    212.3K
+# Total Submissions: 497.4K
+# Testcase Example:  '[1,2,1]'
+#
+# You are visiting a farm that has a single row of fruit trees arranged from
+# left to right. The trees are represented by an integer array fruits where
+# fruits[i] is the type of fruit the i^th tree produces.
+# 
+# You want to collect as much fruit as possible. However, the owner has some
+# strict rules that you must follow:
+# 
+# 
+# You only have two baskets, and each basket can only hold a single type of
+# fruit. There is no limit on the amount of fruit each basket can hold.
+# Starting from any tree of your choice, you must pick exactly one fruit from
+# every tree (including the start tree) while moving to the right. The picked
+# fruits must fit in one of your baskets.
+# Once you reach a tree with fruit that cannot fit in your baskets, you must
+# stop.
+# 
+# 
+# Given the integer array fruits, return the maximum number of fruits you can
+# pick.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: fruits = [1,2,1]
+# Output: 3
+# Explanation: We can pick from all 3 trees.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: fruits = [0,1,2,2]
+# Output: 3
+# Explanation: We can pick from trees [1,2,2].
+# If we had started at the first tree, we would only pick from trees [0,1].
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: fruits = [1,2,3,2,2]
+# Output: 4
+# Explanation: We can pick from trees [2,3,2,2].
+# If we had started at the first tree, we would only pick from trees [1,2].
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= fruits.length <= 10^5
+# 0 <= fruits[i] < fruits.length
+# 
+# 
+#
+
+# @lc code=start
+from typing import List
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        result  = 0
+        slow = 0
+        basket = dict()
+        for fast in range(len(fruits)):
+            if fruits[fast] not in basket: basket[fruits[fast]] = 1
+            else: basket[fruits[fast]] += 1
+            while len(basket)>2:
+                # print(fruits[fast],fruits[slow])
+                # print("basket",basket)
+                if fruits[slow] in basket: del basket[fruits[slow]]
+                slow += 1
+            sublength = sum(basket.values())
+            if result < sublength: result = sublength
+            result = max(result,sublength)
+        return result
+
+s=Solution()
+# print(s.totalFruit([1,2,1]))
+# print(s.totalFruit([0,1,2,2]))
+# print(s.totalFruit([1,2,3,2,2]))
+# print(s.totalFruit([3,3,3,1,2,1,1,2,3,3,4]))
+print(s.totalFruit([4,1,1,1,3,1,7,5]))
+
+
+# @lc code=end
+
