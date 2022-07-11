62ecb8235972286bcafe4cc507a3f0044d77f041
Kewen Li <799150383@qq.com>
2022-07-10 23:23:51 -0700

    Completed 101

---
+++ 101.symmetric-tree.py
@@ -0,0 +1,89 @@
+#
+# @lc app=leetcode id=101 lang=python3
+#
+# [101] Symmetric Tree
+#
+# https://leetcode.com/problems/symmetric-tree/description/
+#
+# algorithms
+# Easy (51.79%)
+# Likes:    10012
+# Dislikes: 243
+# Total Accepted:    1.3M
+# Total Submissions: 2.5M
+# Testcase Example:  '[1,2,2,3,4,4,3]'
+#
+# Given the root of a binary tree, check whether it is a mirror of itself
+# (i.e., symmetric around its center).
+#
+#
+# Example 1:
+#
+#
+# Input: root = [1,2,2,3,4,4,3]
+# Output: true
+#
+#
+# Example 2:
+#
+#
+# Input: root = [1,2,2,null,3,null,3]
+# Output: false
+#
+#
+#
+# Constraints:
+#
+#
+# The number of nodes in the tree is in the range [1, 1000].
+# -100 <= Node.val <= 100
+#
+#
+#
+# Follow up: Could you solve it both recursively and iteratively?
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        return self.compare(root.left, root.right)
+
+    def compare(self, left, right):
+        # if left and (not right):
+        #     return False
+        # elif not(left) and right:
+        #     return False
+        # elif left and right:
+        #     return True
+        if left == None and right != None:
+            return False
+        elif left != None and right == None:
+            return False
+        elif left == None and right == None:
+            return True
+        elif left.val != right.val:
+            return False
+        outside = self.compare(left.left, right.right)
+        inside = self.compare(left.right, right.left)
+        return outside and inside
+
+
+# @lc code=end
